cd2d43e3fefe7b7017c2ded9e69e8c3e0b7f94a0
ivan <ivanrigal@outlook.fr>
2021-02-19 21:35:45 +0100

    v5

--- myapplication.py
+++ myapplication.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! It's orchestrator v2</h1>"
+    return "<h1 style='color:blue'>Hello There! It's orchestrator v4</h1>"
