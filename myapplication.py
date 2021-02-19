14cb8eb58a22dd644139bfdc823f1d815bde1f8e
ivan <ivanrigal@outlook.fr>
2021-02-19 21:40:49 +0100

    v6

--- myapplication.py
+++ myapplication.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+# gogol alert : use git push --tags
+
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There! It's orchestrator v6</h1>"
